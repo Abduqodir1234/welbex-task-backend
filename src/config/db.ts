8576ce06174c305328881a6f9e762beee78ae1cd
Abduqodir1234 <gofurboyevabduqodir@gmail.com>
2022-03-05 14:26:10 +0500

    database config

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,13 +2,13 @@ import {Sequelize} from "sequelize";
 import { configs } from ".";
 import * as dotenv from 'dotenv';
 dotenv.config();
-const db_name:string = process.env.DB_NAME || ''
-const db_user = process.env.DB_USER || ''
-const db_password = process.env.DB_PASS
-export const db = new Sequelize(db_name,db_user,db_password,{...configs.dbconfig,dialect:'postgres'})
+// const db_name:string = process.env.DB_NAME || ''
+// const db_user = process.env.DB_USER || ''
+// const db_password = process.env.DB_PASS
+const db_url = process.env.DATABASE_URL || ''
+export const db = new Sequelize(db_url,{...configs.dbconfig,dialect:'postgres'})
 
 let connectDB = async () =>{
-    console.log(db_name,db_password,db_user)
     try{
         db.authenticate()
         db.sync()  
